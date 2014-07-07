d92625000e9f8415e1d56b837de008ea99ab35cf
Kamilsz <kamilszyc@gmail.com>
2014-07-07 11:41:44 +1000

    Added extra functionaity of self changing colors

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
 
-	
-
-	var height = 4;
-	var width = 4;
+	var height = 20;
+	var width = 20;
 
 	$('button').click(function (){
 
@@ -12,6 +10,9 @@ $(document).ready(function(){
 	};
 	height = window.prompt("Please enter grid height");
 	width = window.prompt("Please enter grid width");
+	if (width>30){
+		width=30;
+	}
 	createGrid(height,width);
 });
 	createGrid(height,width);
@@ -19,16 +20,50 @@ $(document).ready(function(){
 	
 });
 
-
-
+ 	
 function createGrid(height,width){
+
 	for(var i=1; i<=height; i++){
 		$('table').append('<tr id="'+i+'"></tr>')
 		for(var j=1; j<=width; j++){
-			$('#'+i).append('<div class="square"></div>');	
+			$('#'+i).append('<div class="square"></div>');
+
 		};
 	};
+		var r = Math.floor((Math.random() * 255));
+		var g = Math.floor((Math.random() * 255));
+		var b = Math.floor((Math.random() * 255));
+
+		var stepR = Math.floor(r/10);
+		var stepG = Math.floor(g/10);
+		var stepB = Math.floor(b/10);
+
+		var origR=r;
+		var origG=g;
+		var origB=b;
+
+
 	$('.square').hover(function(){
-		$(this).addClass('hover');
+		
+		if (r===0 && g===0 && b===0){
+			change	= true;
+
+		}else if (r>=origR && g>=origG && b>=origB){
+			change = false;
+		}
+		if (!change){
+			r = Math.floor(r-0.1*r);
+			g = Math.floor(g-0.1*g);
+			b = Math.floor(b-0.1*b);
+		} else {
+			r = Math.floor(r+stepR);
+			g = Math.floor(g+stepG);
+			b = Math.floor(b+stepB);
+			
+		}
+		$(this).css("background-color", "rgb("+r+","+g+","+b+")");
+		
 	});
 };
+
+var change=false;
